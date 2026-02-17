d5ddbd9369fc3f04a3b3c9bcc1e7853fa5a63f3d
Junio Rodrigues <jtech.rodrigues@gmail.com>
2026-02-16 21:33:54 -0300

    Implement alarm functionality with user input for setting time and loop for multiple alarms

--- HoraDoPitch/Program.cs
+++ HoraDoPitch/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hora do pitch!");
+﻿using HoraDoPitch;
+
+Alarme alarme = new Alarme();
+
+string resposta = "S";
+while (resposta != "N")
+{
+    Console.Clear();
+    Console.WriteLine("A Hora do Pitch vai começar em breve!");
+    Console.Write("Defina o tempo para o alarme disparar (em minutos): ");
+    int tempo = int.Parse(Console.ReadLine() ?? "0");
+
+    alarme.Tempo = tempo;
+    alarme.Iniciar();
+    Console.WriteLine("Hora do Pitch! Seu tempo acabou!");
+
+    Console.Write("Deseja definir outro alarme? (S/N): ");
+    resposta = Console.ReadLine()?.ToUpper() ?? "N";
+}
+
+Console.WriteLine("Programa encerrado.");
